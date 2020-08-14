f31537512821101bfa4d0e5580e181b632c745d4
isabel@lotus <isabel@izabyl.com>
2020-08-13 21:53:51 -0700

    starting over

--- src/service/storage/Storage.scala
+++
@@ -1,7 +0,0 @@
-package izabyl.rosegarden.service.storage
-
-import izabyl.rosegarden.Service
-
-class Storage extends Service {
-
-}
--- src/service/todo/Todo.scala
+++
@@ -1,7 +0,0 @@
-package izabyl.rosegarden.service.todo
-
-import izabyl.rosegarden.Service
-
-class Todo extends Service {
-
-}
